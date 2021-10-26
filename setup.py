037e512d7e36aaac1fdd6a1554a823c94c051aac
adrianperez <adrianperez@stu.postoakschool.org>
2021-10-26 10:16:09 -0500

    added files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ButtonMashingGame',
+    version='Beta.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='adrianperez',
+    author_email='adrianperez@stu.postoakschool.org',
+    description='',
+    install_requires = [
+        'getch~=1.0',
+        'pygame~=2.0.2',
+        'setuptools~=58.0.4'
+    ]
+)
